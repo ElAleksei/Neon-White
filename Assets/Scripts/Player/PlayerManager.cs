466c2ebb13cf3e6b90c98e906a97a4f4a6b99df7
ElAleksei <alekseigames10@gmail.com>
2023-02-23 22:07:02 -0600

    Animaciones y texturas +Implementación de más modelos y props +Texturas de los anteriores +Skybox añadida

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
     public int m_PlayerLife;
+    public GameObject m_Life1;
+    public GameObject m_Life2;
+    public GameObject m_Life3;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +30,25 @@ public class PlayerManager : MonoBehaviour
         if (collision.gameObject.tag == "Bullet")
         {
             m_PlayerLife -= 1;
+            ChangeLife();
+        }
+    }
+
+    void ChangeLife()
+    {
+        if(m_PlayerLife == 2)
+        {
+            m_Life3.GetComponent<Image>().color = Color.black;
+        }
+
+        if (m_PlayerLife == 1)
+        {
+            m_Life2.GetComponent<Image>().color = Color.black;
+        }
+
+        if (m_PlayerLife == 0)
+        {
+            m_Life1.GetComponent<Image>().color = Color.black;
         }
     }
 
